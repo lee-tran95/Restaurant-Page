294b60a7a8e096e739165cdb3ccc53ac5c785302
Lee Tran <lee.tran95@yahoo.com>
2023-08-10 20:45:43 -0700

    Replace background photo and add an order now button

--- src/modules/home.js
+++ src/modules/home.js
@@ -2,14 +2,17 @@
 function loadHome(){
     const homeDiv = document.createElement('div')
     homeDiv.classList.add('homeContent')
-    
+
     const restaurantAboutHeader = document.createElement('h4')
     restaurantAboutHeader.textContent = "SUSHIDON IS..."
 
     const restaurantAbout = document.createElement('p')
     restaurantAbout.textContent = "SushiDon provides hand crafted udon bowls, tempura, sushi and beverages from a theater kitchen, allowing guests to watch as their food is prepared.Our udon noodles and sushi are made to order to ensure that you are getting the best tasting Japanese food around."
 
-    homeDiv.append(restaurantAboutHeader, restaurantAbout)
+    const orderButton = document.createElement('Button')
+    orderButton.textContent = "ORDER NOW"
+
+    homeDiv.append(restaurantAboutHeader, restaurantAbout, orderButton)
     document.getElementById('content').append(homeDiv)
 }
 

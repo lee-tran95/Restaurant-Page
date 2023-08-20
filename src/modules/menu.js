8a98c3887fe77f49c80aa5382da2b8902bf426dc
Lee Tran <lee.tran95@yahoo.com>
2023-08-19 23:02:08 -0700

    Create a css file for the menu tab and create structure for the menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,7 +1,12 @@
-
+ import '../styles/menu.css'
 function loadMenu(){
     const menuDiv = document.createElement('div')
-    menuDiv.textContent = "Hi"
+    menuDiv.id = 'menu-content'
+
+    const menuItems = document.createElement('div')
+    menuItems.classList.add('menu-items')
+
+    menuDiv.append(menuItems)
     document.getElementById('content').append(menuDiv)
 }
 

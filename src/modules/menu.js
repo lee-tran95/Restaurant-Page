b7f1e401ba5a13bab76c73c1d0f567e858def27e
Lee Tran <lee.tran95@yahoo.com>
2023-08-20 23:23:12 -0700

    Create a function in the menu module that creates menu items given a name and description

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,9 +5,33 @@ function loadMenu(){
 
     const menuItems = document.createElement('div')
     menuItems.classList.add('menu-items')
+    
+    menuItems.appendChild(
+        createMenuItem(
+            "Udon",
+            "Delcious thick noodles in a sauce of your choice"
+        )
+    )
 
     menuDiv.append(menuItems)
     document.getElementById('content').append(menuDiv)
+}
+
+function createMenuItem(name, description){
+    const menuItem = document.createElement('div')
+    menuItem.classList.add('menu-item')
+
+    const itemName = document.createElement('h2')
+    itemName.classList.add('item-name')
+    itemName.textContent = `${name}`
+
+    const itemDescription = document.createElement('p')
+    itemDescription.classList.add('item-description')
+    itemDescription.textContent = `${description}`
+
+    menuItem.append(itemName, itemDescription)
+
+    return menuItem
 }
 
 export default loadMenu
